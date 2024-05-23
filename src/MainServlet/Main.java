2c7a6563392771ea8a1f19934142325e6ddf8d09
kohe.kurose <user029.dounyu@techacademy.jp>
2024-05-23 10:52:47 +0900

    editの改善

--- src/MainServlet/Main.java
+++ src/MainServlet/Main.java
@@ -36,6 +36,7 @@ public class Main extends HttpServlet {
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtils.createEntityManager();
 
@@ -48,6 +49,7 @@ public class Main extends HttpServlet {
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
         doGet(request, response);
