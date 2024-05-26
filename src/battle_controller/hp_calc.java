ebcd8f951122e642158714a7f4a82ce6934164ad
koki.sawano <user045.dounyu@techacademy.jp>
2024-05-26 23:04:16 +0900

    aaa

--- src/battle_controller/hp_calc.java
+++ src/battle_controller/hp_calc.java
@@ -2,10 +2,16 @@ package battle_controller;
 
 public class hp_calc {
 
-	public static int enemy_hp_calc(int damage, int hp) {
+    public static int hero_hp_calc(int damage, int hp) {
             hp-=damage;
             System.out.println("hero_hp_calcを完了しました。");
         return hp;
+    }
+
+    public static int enemy_hp_calc(int damage, int hp) {
+        hp-=damage;
+        System.out.println("hero_hp_calcを完了しました。");
+    return hp;
     }
 
 }
