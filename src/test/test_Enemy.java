ca48976bacd4a9954e967b505c3a771fbdde3321
koki.sawano <user045.dounyu@techacademy.jp>
2024-05-28 02:34:31 +0900

    修正

--- src/test/test_Enemy.java
+++ src/test/test_Enemy.java
@@ -1,8 +1,10 @@
 package test;
 
+import DTO.enemy;
+
 //import test.Character;
 
-public class test_Enemy extends DTO.enemy  {
+public class test_Enemy extends enemy  {
 
     public test_Enemy(Integer id, String name, int level, int speed, String finish_ward, int attack_value, int hp, int max_hp) {
         super(id, name, level, speed, finish_ward, attack_value, hp,max_hp);
@@ -14,7 +16,7 @@ public class test_Enemy extends DTO.enemy  {
         // TODO 自動生成されたコンストラクター・スタブ
     }
 
-    test_Enemy b = new test_Enemy();
+    enemy b = new enemy();
 
     public test_Enemy() {
 
